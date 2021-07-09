2e94263c092e01c40a78a4b1bebfa7c441ee09a7
chatastaff <83054570+chatastaff@users.noreply.github.com>
2021-07-09 16:33:47 -0700

    Update matthe.js

--- matthe.js
+++ matthe.js
@@ -5,9 +5,9 @@ tokens.forEach((token, i) => {
   const client = new Client();
   let connection;
   client.on("ready", async () => connection = await client.channels.cache.get(channels[i]).join());
-
+    
   client.on("voiceStateUpdate", async (oldState, newState) => {
-    if ((oldState.channelID && !newState.channelID)&&(oldState.channelID && newState.channelID && oldState.channelID === newState.channelID)&&newState.member.user.bot || newState.channelID !== channels[i]) return;
+    if ((oldState.channelID && !newState.channelID) || (oldState.channelID && newState.channelID && oldState.channelID === newState.channelID) || newState.member.user.bot || newState.channelID !== channels[i]) return;
     const hasStaff = newState.channel.members.some((x)=> staffRoles.some((r) => x.roles.cache.has(r)));
     const staffSize = newState.channel.members.filter((x) => staffRoles.some((r) => x.roles.cache.has(r))).size;
     const unregisterSize = newState.channel.members.filter((x) => WelcomeS.some((r) => x.roles.cache.has(r))).size;
@@ -15,9 +15,9 @@ tokens.forEach((token, i) => {
     else if (hasStaff && staffSize === 1 && unregisterSize === 1) await connection.play(staffSound);
   });
 
-
+  
   client.on("ready", async () => {
-  client.user.setPresence({ activity: { name: "ASHKAN UP " }, status: "idle" });
+  client.user.setPresence({ activity: { name: "ASHKAN UP 🖤" }, status: "idle" });
   })
   client.login(token).then(() => console.log(`${client.user.tag} Aktif!`)).catch(() => console.error(`${token} !`));
 });
